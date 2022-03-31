36b411b8e6738826e5b03786212f1a6727b58f87
Imfckngbtfl <102735294+Imfckngbtfl@users.noreply.github.com>
2022-03-31 18:09:31 +0300

    1.0002

---
+++ Controllers/Game_controller.py
@@ -0,0 +1,3 @@
+class Game_controller():
+    def start(self):
+        pass
---
+++ UI/Map_drawer.py
@@ -0,0 +1,5 @@
+class Map_drawer():
+    def __init__(self):
+        pass
+    def draw_map(self, cells):
+        #Принимает список обновлённых клеток и отрисовывает их по их координатам
