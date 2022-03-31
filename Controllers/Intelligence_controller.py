a52d55f55125667fa27143d42b09a98ced9126b9
Imfckngbtfl <102735294+Imfckngbtfl@users.noreply.github.com>
2022-03-31 16:38:13 +0300

    Update

--- Controllers/Intelligence_controller.py
+++ Controllers/Intelligence_controller.py
@@ -1,8 +1,9 @@
-class Intelligence_controller(Intelligence):
-    def __init__(self):
-        self.Intelligenece = Intelligence
+import Cell
+class Intelligence_controller():
+    def __init__(self, Cell):
+        self.Intelligenece = Cell.Get_object().Intelligence
         self.Current = 0
-    self.Dict = {0:"f",1:"fr",2:"r",3:"b"}
+    self.Dict = {0:"f",1:"fr",2:"r",3:"b",4:""}
     def Do(self):
         if self.Intelligenece[self.Current] <=8:
             Move_controller()
--- Engine/Cell.py
+++ Engine/Cell.py
@@ -2,8 +2,10 @@ import Creature.py
 import Berry.py
 import Mineral.py
 class Cell():
-    def __init__(self, object = NULL):
+    def __init__(self, object = NULL, x, y):
         self.object = object
+        self.x = x
+        self.y = y
     def Set_object(self, object):
         self.object = object
     def Get_object(self):
